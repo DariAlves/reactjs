139e4965bc6539e13423148e17fd516974813aef
Dari Alves <darieltondf@gmail.com>
2021-11-28 20:14:36 -0300

    Card component

--- movie-list/src/containers/List.js
+++ movie-list/src/containers/List.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
+import { Card } from '../components/Card/Card';
 
-class List extends Component {
+export class List extends Component {
     render() {
-        return <h1>Movie List</h1>
+        return <Card />
     }
 };
-
-export default List;

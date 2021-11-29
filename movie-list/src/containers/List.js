6c1b6f8afe7f63644728419a8b66e97047352085
Dari Alves <darieltondf@gmail.com>
2021-11-28 21:14:56 -0300

    List.js functional component

--- movie-list/src/containers/List.js
+++ movie-list/src/containers/List.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { Card } from '../components/Card/Card';
 
-export class List extends Component {
-    render() {
-        return <Card />
-    }
-};
+
+export const List = () => {
+    return <Card />;
+}
+
+// export class List extends Component {
+//     render() {
+//         return <Card />
+//     }
+// };
